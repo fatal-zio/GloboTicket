a9f6848616eeb67dc065311cdf27c741ab44f820
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-02-14 17:50:14 -0600

    Created Solution and Domain project.

---
+++ TicketManagement/Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Event>? Events { get; set; }
+    }
+}
