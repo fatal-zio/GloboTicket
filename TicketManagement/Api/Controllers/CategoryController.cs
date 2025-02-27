6dd8be78329fd7f4a6bec1f96c1435f394531ad2
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-02-27 17:59:43 -0600

    Added Mediator and Category Controller.

---
+++ TicketManagement/Api/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.Categories.Commands.CreateCategory;
+using Application.Features.Categories.Queries.GetCategoriesList;
+using Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpGet("all", Name = "GetAllCategories")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryListViewModel>>> GetAllCategories()
+        {
+            var categoryDtos = await _mediator.Send(new GetCategoriesListQuery());
+            return Ok(categoryDtos);
+        }
+
+        [HttpGet("allwithevents", Name = "GetCategoriesWithEvents")]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<CategoryEventListViewModel>>> GetCategoriesWithEvents(bool includeHistory)
+        {
+            GetCategoriesListWithEventsQuery getCategoriesListWithEventsQuery = new() { IncludeHistory = includeHistory };
+
+            var categoriesWithEventsDtos = await _mediator.Send(getCategoriesListWithEventsQuery);
+            return Ok(categoriesWithEventsDtos);
+        }
+
+        [HttpPost(Name = "AddCategory")]
+        public async Task<ActionResult<CreateCategoryCommandResponse>> Create([FromBody] CreateCategoryCommand createCategoryCommand)
+        {
+            return Ok(await _mediator.Send(createCategoryCommand));
+        }
+    }
+}
